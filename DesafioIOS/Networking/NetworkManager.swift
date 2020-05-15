a184d3c8b5a7e3c9d23cdae0437d209cc6d0838d
Walter <walteroliveira@support.diamondbybold.com>
2020-05-15 13:58:18 -0300

    Add Basic Networking

---
+++ DesafioIOS/Networking/NetworkManager.swift
@@ -0,0 +1,34 @@
+//
+//  APIService.swift
+//  DesafioIOS
+//
+//  Created by Walter Oliveira on 15/05/20.
+//  Copyright © 2020 Walter Oliveira. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    static let eventListUrl = URL(string: "http://5b840ba5db24a100142dcd8c.mockapi.io/api/events")!
+    
+    func load(url: URL, withCompletion completion: @escaping (Data?) -> Void) {
+        let session = URLSession(configuration: .ephemeral, delegate: nil, delegateQueue: .main)
+        let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            completion(data)
+        })
+        task.resume()
+    }
+    
+    func loadEventList(completion: @escaping ([EventModel]?) -> Void) {
+        load(url: NetworkManager.eventListUrl) { data in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            let eventList = try? JSONDecoder().decode([EventModel].self, from: data)
+            completion(eventList)
+        }
+    }
+    
+}
