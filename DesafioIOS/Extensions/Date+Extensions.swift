44ea3bc37f762e96a4ff2408d0b6bbd84083e1ee
Walter <walteroliveira@support.diamondbybold.com>
2020-05-15 15:11:47 -0300

    Add Extensions

---
+++ DesafioIOS/Extensions/Date+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  Date+Extensions.swift
+//  DesafioIOS
+//
+//  Created by Walter Oliveira on 15/05/20.
+//  Copyright © 2020 Walter Oliveira. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+
+    func toStringDate() -> String{
+        let dataFormatter = DateFormatter()
+        dataFormatter.dateFormat = "dd/MM/YYYY"
+        
+        return dataFormatter.string(from: self)
+    }
+    
+}
